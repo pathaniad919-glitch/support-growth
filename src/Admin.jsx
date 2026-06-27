44e3ad47ffacdc77d85566b670aac45d53886851
Deepak Pathania <deepakpathania@Deepaks-MacBook-Air.local>
2026-06-27 18:47:23 +0530

    added lead notification sound

--- src/Admin.jsx
+++ src/Admin.jsx
@@ -1,9 +1,24 @@
 import { useEffect, useState } from "react";
 import { db } from "./firebase";
-import { collection, onSnapshot } from "firebase/firestore";
-
+import {
+  collection,
+  onSnapshot,
+  doc,
+  updateDoc,
+  deleteDoc
+} from "firebase/firestore";
 export default function Admin() {
   const [leads, setLeads] = useState([]);
+  const audio = new Audio("/notification.mp3");
+  const updateStatus = async (id, status) => {
+  await updateDoc(doc(db, "leads", id), {
+    status,
+  });
+};
+
+const deleteLead = async (id) => {
+  await deleteDoc(doc(db, "leads", id));
+};
 
   useEffect(() => {
   const unsubscribe = onSnapshot(collection(db, "leads"), (snapshot) => {
@@ -12,11 +27,16 @@ export default function Admin() {
       ...doc.data(),
     }));
 
+    if (leadList.length > leads.length) {
+      audio.play();
+      alert("New Lead Arrived!");
+    }
+
     setLeads(leadList);
   });
 
   return () => unsubscribe();
-}, []);
+}, [leads]);
 
   return (
   <div
@@ -45,6 +65,19 @@ export default function Admin() {
         >
           <p><strong>Name:</strong> {lead.name}</p>
 <p><strong>Phone:</strong> {lead.phone}</p>
+<p><strong>Status:</strong> {lead.status || "new"}</p>
+
+<button onClick={() => updateStatus(lead.id, "picked")}>
+  Picked
+</button>
+
+<button onClick={() => updateStatus(lead.id, "redial")}>
+  Redial
+</button>
+
+<button onClick={() => deleteLead(lead.id)}>
+  Delete
+</button>
         </div>
       ))
     )}
