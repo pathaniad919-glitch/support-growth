4eafe8aa190b3d9c12d8ff3a5b8f9a6b86aa22ff
Deepak Pathania <deepakpathania@Deepaks-MacBook-Air.local>
2026-06-27 16:26:23 +0530

    fix admin

--- src/Admin.jsx
+++ src/Admin.jsx
@@ -1,21 +1,41 @@
-return (
-  <div
-    style={{
-      padding: "40px",
-      background: "black",
-      color: "white",
-      minHeight: "100vh"
-    }}
-  >
-    <h1>Leads Page Working</h1>
+import { useEffect, useState } from "react";
+import { db } from "./firebase";
+import { collection, getDocs } from "firebase/firestore";
 
-    <p>Total Leads: {leads.length}</p>
+export default function Admin() {
+  const [leads, setLeads] = useState([]);
 
-    {leads.length > 0 &&
-      leads.map((lead) => (
-        <div key={lead.id}>
+  useEffect(() => {
+    const fetchLeads = async () => {
+      const querySnapshot = await getDocs(collection(db, "leads"));
+
+      const leadList = querySnapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+
+      setLeads(leadList);
+    };
+
+    fetchLeads();
+  }, []);
+
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>All Leads</h1>
+
+      {leads.map((lead) => (
+        <div
+          key={lead.id}
+          style={{
+            border: "1px solid #ccc",
+            padding: "15px",
+            marginBottom: "10px",
+          }}
+        >
           <pre>{JSON.stringify(lead, null, 2)}</pre>
         </div>
       ))}
-  </div>
-);
+    </div>
+  );
+}
