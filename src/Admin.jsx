ddf85fbadd4508b35e93bc3f89f36d2f9fb5e14b
Deepak Pathania <deepakpathania@Deepaks-MacBook-Air.local>
2026-06-27 15:34:07 +0530

    Fixed Admin page

---
+++ src/Admin.jsx
@@ -0,0 +1,46 @@
+import { useEffect, useState } from "react";
+import { db } from "./firebase";
+import { collection, getDocs } from "firebase/firestore";
+
+function Admin() {
+  const [leads, setLeads] = useState([]);
+
+  useEffect(() => {
+    const fetchLeads = async () => {
+      const querySnapshot = await getDocs(
+        collection(db, "leads")
+      );
+
+      const leadList = querySnapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+
+      setLeads(leadList);
+    };
+
+    fetchLeads();
+  }, []);
+
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>All Leads</h1>
+
+      {leads.map((lead) => (
+        <div
+          key={lead.id}
+          style={{
+            border: "1px solid #ccc",
+            padding: "15px",
+            marginBottom: "10px",
+          }}
+        >
+          <p><strong>Lead ID:</strong> {lead.leadId}</p>
+          <pre>{JSON.stringify(lead.leadData, null, 2)}</pre>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default Admin;
