207c721059b092dc36af5f62a89864a59c77cbb4
Deepak Pathania <deepakpathania@Deepaks-MacBook-Air.local>
2026-08-19 00:15:29 +0530

    Fix video fullscreen

--- src/App.jsx
+++ src/App.jsx
@@ -1648,7 +1648,7 @@ function PremiumAccessPage() {
   src="https://www.dropbox.com/scl/fi/l6sqpvk4aza4bt2veaz5v/1784225170367200.MP4?rlkey=e6jx0pjdzpnt7ax97vt8cka0j&st=ra0d1low&raw=1"
   title="Premium Training"
   frameBorder="0"
-  allow="autoplay; fullscreen"
+  allow="autoplay; fullscreen; picture-in-picture"
   allowFullScreen
   style={{
     position: "absolute",
